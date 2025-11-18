71748b5400ea6c360340eb84cd109b8d7725ac85
IronTenshi <2356128385@qq.com>
2025-11-18 21:36:44 +0800

    Create ValueDeemo1.java

---
+++ ValueDeemo1.java
@@ -0,0 +1,11 @@
+public class ValueDeemo1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("asdasdasd");
+        System.out.println('a');
+        System.out.println('从');
+        System.out.println(1);
+        System.out.println(true);
+    }
+}
