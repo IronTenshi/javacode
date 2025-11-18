1e7c76d14d2b4464e8d622e9e884b77a1b24f4a4
IronTenshi <2356128385@qq.com>
2025-11-18 22:05:35 +0800

    Create vlauedeemo2.java

---
+++ vlauedeemo2.java
@@ -0,0 +1,21 @@
+public class vlauedeemo2 {
+    public static void main(String[] args) {
+        byte b = 100; // 2^8;
+        System.out.println(b);
+        short a = 123;
+        System.out.println(a);
+        int i = 100;
+        System.out.println(i);
+        //if you wanna use long 
+        //the t should be added   'L;
+        long t = 1999000007777234770L;
+        System.out.println(t);
+        //liked long using F;
+        float u = 10.123F;
+        System.out.println(u);
+        double asd =12397834.123;
+        System.out.println(asd);
+        char c = '在';
+        System.out.println(c);
+    }
+}
